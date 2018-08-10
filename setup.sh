d95e7a24fbbcbe60ec450c2c02cfc6c44debc881
S8Cloud <github@live.hk>
2018-08-10 16:26:37 +0800

    fix pip related problems

--- setup.sh
+++ setup.sh
@@ -36,6 +36,7 @@ EOF
 }
 lemp_install(){
     apt-get install vim git sudo unzip screen dialog gnupg2 htop mtr nload hdparm python-pip python-dev python3-pip python3-dev build-essential openssl libelf-dev libffi-dev libssl-dev fail2ban libsodium-dev nginx-light php7.2-fpm php7.2-mysql php7.2-curl php7.2-gd php7.2-mbstring php7.2-xml php7.2-xmlrpc php7.2-zip php7.2-opcache -y
+    pip3 install pip -U && pip2 install pip -U && hash -r
     curl -sL https://deb.nodesource.com/setup_8.x | bash -
     apt update && apt install nodejs -y && npm i -g npm yarn pm2
     apt autoremove && apt autoclean
@@ -107,8 +108,7 @@ EOF
 }
 ssr_install(){
     echo "sshd: ALL" > /etc/hosts.allow
-    pip3 install pip -U && pip2 install pip -U && hash -r
-    pip2 install setuptools cymysql==0.9.4 requests pyasn1 ndg-httpsclient pyOpenSSL urllib3 cryptography speedtest-cli -U
+    pip install setuptools -U && pip2 install cryptography cymysql==0.9.4 requests pyasn1 ndg-httpsclient pyOpenSSL urllib3 cryptography speedtest-cli
     cd /root/
     git clone https://github.com/S8Cloud/shadowsocks.git && cd shadowsocks
     cat > userapiconfig.py << EOF
