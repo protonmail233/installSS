e77c53adf6c13dbb38b4697ddb88c08122595e3e
S8Cloud <github@live.hk>
2018-10-04 09:38:51 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,7 +16,6 @@ EOF
         wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
         sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
         apt-get update && apt-get dist-upgrade -y 
-
     elif [[ "${ID}" == "ubuntu" && `echo "${VERSION_ID}" | cut -d '.' -f1` -ge 16 ]];then
         apt-get install lsb-release apt-transport-https wget curl ca-certificates language-pack-en -y
         cat > /etc/apt/sources.list << EOF
@@ -36,13 +35,13 @@ EOF
 }
 lemp_install(){
     apt-get purge apache* -y
-    apt-get install vim git sudo unzip screen dialog gnupg2 htop mtr nload hdparm python-pip python-dev python3-pip python3-dev build-essential openssl libelf-dev libffi-dev libssl-dev fail2ban libsodium-dev nginx-light php7.2-fpm php7.2-mysql php7.2-curl php7.2-gd php7.2-mbstring php7.2-xml php7.2-xmlrpc php7.2-zip php7.2-opcache -y
+    apt-get install vim git sudo unzip screen dialog gnupg2 htop mtr nload hdparm python-pip python-dev python3-pip python3-dev build-essential openssl libelf-dev libffi-dev libssl-dev fail2ban libsodium-dev nginx-light php7.2-fpm php7.2-mysql php7.2-curl php7.2-gd php7.2-mbstring php7.2-xml php7.2-xmlrpc php7.2-zip php7.2-opcache neofetch -y
     pip3 install pip -U && pip2 install pip -U && hash -r
     curl -sL https://deb.nodesource.com/setup_8.x | bash -
     curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
     echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
     apt-get update && apt-get install nodejs yarn -y && yarn global add pm2
-    apt autoremove && apt autoclean
+    apt autoremove -y && apt autoclean -y
     echo "* soft nofile 512000" >> /etc/security/limits.conf
     echo "* hard nofile 512000" >> /etc/security/limits.conf
     ulimit -n 512000
@@ -113,7 +112,7 @@ ssr_install(){
     echo "sshd: ALL" > /etc/hosts.allow
     pip install setuptools -U && pip install cryptography pyOpenSSL -U && pip install cymysql requests pyasn1 ndg-httpsclient urllib3 speedtest-cli
     cd /root/ && git clone https://github.com/S8Cloud/shadowsocks.git && cd shadowsocks && vi userapiconfig.py
-    pm2 start server.json && pm2 startup && pm2 save && cd /root/ && curl ipv4.ip.sb
+    pm2 start server.json && pm2 startup && pm2 save && cd /root/ && curl ipv4.ip.sb && neofetch
 }
 
 action=$1
