4f3a28169c01f61f123150e3ca312c50932a88fc
S8Cloud <github@live.hk>
2018-10-13 11:50:54 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 check_system(){
     source /etc/os-release
     if [[ "${ID}" == "debian" && ${VERSION_ID} -ge 8 ]];then
-        apt-get update && apt-get install lsb-release wget curl ca-certificates apt-transport-https locales locales-all -y
+        apt-get update && apt-get install lsb-release wget curl ca-certificates apt-transport-https locales-all -y
         cat > /etc/apt/sources.list << EOF
 deb http://mirror.rackspace.com/debian $(lsb_release -sc) main contrib non-free
 deb http://mirror.rackspace.com/debian-security $(lsb_release -sc)/updates main contrib non-free
@@ -85,7 +85,8 @@ vm.swappiness = 10
 vm.vfs_cache_pressure = 50
 net.ipv4.ip_forward = 1
 net.ipv4.icmp_echo_ignore_all = 1
-net.core.default_qdisc = fq_codel
+net.core.default_qdisc = fq
+net.ipv4.tcp_notsent_lowat = 16384
 net.ipv4.tcp_congestion_control = bbr
 net.ipv4.tcp_fastopen = 3
 EOF
